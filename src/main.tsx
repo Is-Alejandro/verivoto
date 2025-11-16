5322dcebb6063e2b9edfef67d26d02472a9c63c7
Is-Alejandro <U22302560@utp.edu.pe>
2025-11-15 19:55:46 -0500

    e configuró el PWA y ajustes en Vite

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppRouter from './router/AppRouter'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  ],
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'VeriVoto',
+        short_name: 'VeriVoto',
+        description: 'Aplicación informativa para votantes y miembros de mesa.',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#1A73E8',
+        icons: [
+          {
+            src: 'src/assets/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'src/assets/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
 })
